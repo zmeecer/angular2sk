a0a5120045a7b3ec6a3b0726c3ef6c54e2ddcb7c
Źmicier Jaraševič <dzmitry.yaroshevich@azati.com>
2016-02-15 10:34:40 +0300

    updated content

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component} from 'angular2/core';
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>My First Angular 2 App, bla</h1><p>test</p>'
 })
 export class AppComponent { }
